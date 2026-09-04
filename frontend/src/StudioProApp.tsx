6b1173ee641ef249e08b3395ab63f8e47f6b0972
track2000one <track2000one@gmail.com>
2026-09-05 00:20:25 +0300

    feat: show live project save status in Studio Pro

--- frontend/src/StudioProApp.tsx
+++ frontend/src/StudioProApp.tsx
@@ -1,10 +1,12 @@
 import StudioCommandPalette from './StudioCommandPalette'
+import StudioProjectStatus from './StudioProjectStatus'
 import VideoStudioCreator from './VideoStudioCreator'
 
 export default function StudioProApp() {
   return (
     <>
       <VideoStudioCreator />
+      <StudioProjectStatus />
       <StudioCommandPalette />
     </>
   )
