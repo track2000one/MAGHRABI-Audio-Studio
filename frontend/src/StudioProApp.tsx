677423cd9f32a1e00138d868414ca10546f8331c
track2000one <track2000one@gmail.com>
2026-09-05 00:48:47 +0300

    feat: enable high-contrast Studio Pro and draggable playhead

--- frontend/src/StudioProApp.tsx
+++ frontend/src/StudioProApp.tsx
@@ -1,13 +1,16 @@
 import StudioCommandPalette from './StudioCommandPalette'
 import StudioProjectStatus from './StudioProjectStatus'
+import StudioTimelineEnhancer from './StudioTimelineEnhancer'
 import VideoStudioCreator from './VideoStudioCreator'
+import './studioProContrast.css'
 
 export default function StudioProApp() {
   return (
-    <>
+    <div className="maghrabi-studio-pro min-h-screen">
       <VideoStudioCreator />
+      <StudioTimelineEnhancer />
       <StudioProjectStatus />
       <StudioCommandPalette />
-    </>
+    </div>
   )
 }
