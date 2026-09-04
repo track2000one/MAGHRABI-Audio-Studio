136bbd63ce1925e5aa5a5a2f061383fd11049c0a
track2000one <track2000one@gmail.com>
2026-09-05 00:14:02 +0300

    feat: compose Studio Pro editor and command system

---
+++ frontend/src/StudioProApp.tsx
@@ -0,0 +1,11 @@
+import StudioCommandPalette from './StudioCommandPalette'
+import VideoStudioCreator from './VideoStudioCreator'
+
+export default function StudioProApp() {
+  return (
+    <>
+      <VideoStudioCreator />
+      <StudioCommandPalette />
+    </>
+  )
+}
